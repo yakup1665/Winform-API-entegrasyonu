5bd5198c1100d18dfbc16f18845d653ddc4ad857
Yakup Daş <dasyakup@outlook.com>
2025-12-17 15:58:08 +0300

    proje tammalandı

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace ApıİleVeriCekme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVeriCek = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnVeriCek
+            // 
+            this.btnVeriCek.Location = new System.Drawing.Point(1352, 12);
+            this.btnVeriCek.Name = "btnVeriCek";
+            this.btnVeriCek.Size = new System.Drawing.Size(111, 54);
+            this.btnVeriCek.TabIndex = 0;
+            this.btnVeriCek.Text = "Bağlan";
+            this.btnVeriCek.UseVisualStyleBackColor = true;
+            this.btnVeriCek.Click += new System.EventHandler(this.btnVeriCek_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonShadow;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 131);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1475, 635);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1475, 766);
+            this.Controls.Add(this.btnVeriCek);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVeriCek;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace ApıİleVeriCekme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class post
+        {
+            public int Id { get; set; }
+            public String Title { get; set; }
+            public String Body { get; set; }
+        }
+
+        private async void btnVeriCek_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                btnVeriCek.Enabled = false;
+                btnVeriCek.Text = "Bağlanıyor...";
+                string url = "https://jsonplaceholder.typicode.com/posts";
+                using (HttpClient client = new HttpClient())
+
+                {
+                    string jsonResult = await client.GetStringAsync(url); // Burada veriyi çektim
+                    GridiGuzellestir(dataGridView1);
+                    List<post> postlar = JsonConvert.DeserializeObject<List<post>>(jsonResult);// burada indiridiğim nuget paketi olan network.json paketi ile üst satırda çektiğimiz bilgileri listeliyorum
+                    dataGridView1.DataSource = postlar;//burada datagridimize bağladım
+                    dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                    dataGridView1.Columns["Id"].Width = 50;
+                    dataGridView1.Columns["Title"].HeaderText = "Başlık";
+                    dataGridView1.Columns["Body"].HeaderText = "İçerik";
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bağlantı Hatası:" + ex.Message);
+
+            }
+            finally
+            {
+                btnVeriCek.Enabled = true;
+                btnVeriCek.Text = "VeriÇek";
+            }
+        }
+        private void GridiGuzellestir(DataGridView dgv)
+        {
+            // 1. Genel Renk ve Kenarlık Ayarları
+            dgv.BackgroundColor = Color.White;
+            dgv.BorderStyle = BorderStyle.None;
+            dgv.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dgv.GridColor = Color.FromArgb(230, 230, 230); // Açık gri çizgi
+            dgv.EnableHeadersVisualStyles = false;
+            dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgv.MultiSelect = false;
+            dgv.AllowUserToResizeRows = false;
+            dgv.ReadOnly = true;
+
+            // 2. Başlık (Header) Tasarımı
+            DataGridViewCellStyle headerStyle = new DataGridViewCellStyle();
+            headerStyle.BackColor = Color.FromArgb(44, 62, 80); // Koyu lacivert/gri tonu
+            headerStyle.ForeColor = Color.White;
+            headerStyle.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            headerStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dgv.ColumnHeadersDefaultCellStyle = headerStyle;
+            dgv.ColumnHeadersHeight = 45;
+            dgv.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+
+            // 3. Satır Tasarımı ve Zebra Efekti
+            dgv.RowTemplate.Height = 40;
+            dgv.DefaultCellStyle.Font = new Font("Segoe UI", 9);
+            dgv.DefaultCellStyle.ForeColor = Color.FromArgb(64, 64, 64);
+            dgv.DefaultCellStyle.SelectionBackColor = Color.FromArgb(52, 152, 219); // Şık mavi seçim rengi
+            dgv.DefaultCellStyle.SelectionForeColor = Color.White;
+
+            // Alternatif (Zebra) satır rengi
+            dgv.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(248, 249, 250);
+
+            // 4. Diğer Ayarlar
+            dgv.RowHeadersVisible = false; // Sol boşluğu kaldır
+            dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill; // Tam sığdır
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GridiGuzellestir(dataGridView1);
+        }
+
+
+    }
+}
